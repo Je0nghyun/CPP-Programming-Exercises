36dc8d435670cd9bbac845dad698da070b4ad04e
Je0nghyun <103135590+Je0nghyun@users.noreply.github.com>
2022-05-20 21:49:44 +0900

    Add files via upload

---
+++ Week9_Homework2/VariousMonsters.h
@@ -0,0 +1,129 @@
+#pragma once
+#include "Monster.h"
+
+class Zombie : public Monster {
+public:
+	Zombie(string n = "허접좀비", string i = "§", int x = 0, int y = 0)
+		: Monster(n, i, x, y) {}
+	~Zombie() { cout << " Zombie"; }
+};
+
+class Vampire : public Monster {
+public:
+	Vampire(string n = "뱀파이어", string i = "★", int x = 0, int y = 0)
+		: Monster(n, i, x, y) {}
+	~Vampire() { cout << " Vampire"; }
+
+	void move(int** map, int maxx, int maxy) {
+		int dir = rand() % 4;
+		if (dir == 0) x--;
+		else if (dir == 1) x++;
+		else if (dir == 2) y--;
+		else y++;
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
+
+class KGhost : public Monster {
+public:
+	KGhost(string n = "처녀귀신", string i = "♥", int x = 0, int y = 0)
+		: Monster(n, i, x, y) {}
+	~KGhost() { cout << " KGhost"; }
+
+	void move(int** map, int maxx, int maxy) {
+		x = rand() % maxx;
+		y = rand() % maxy;
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
+
+class Jiangshi : public Monster {
+	bool bHori;
+public:
+	Jiangshi(string n = "대륙강시", string i = "↔", int x = 0, int y = 0, bool bH = true)
+		: Monster(n, i, x, y), bHori(bH) {}
+	~Jiangshi() { cout << " Jiangshi"; }
+
+	void move(int** map, int maxx, int maxy) {
+		int dir = rand() % 2;
+		int jump = rand() % 2 + 1;
+		if (bHori) x += ((dir == 0) ? -jump : jump);
+		else y += ((dir == 0) ? -jump : jump);
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
+
+class Smombi : public Monster {
+public:
+	Smombi(string n = "스몸비", string i = "※", int x = 0, int y = 0)//추가
+		: Monster(n, i, x, y) {}
+	~Smombi() { cout << " Smombi"; }
+
+	void move(int** map, int maxx, int maxy) {
+		int dir = rand() % 4;
+		if (dir == 0) { x--; y--; }
+		else if (dir == 1) { x++; y++; }
+		else if (dir == 2) { x++; y--; }
+		else { x--; y++; }
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
+
+class Siangshi : public Jiangshi {
+	int nMove = 0;
+public:
+	Siangshi(string n = "수퍼강시", string i = "○", int x = 0, int y = 0)//추가
+		: Jiangshi(n, i, x, y) {}
+	~Siangshi() { cout << " Siangshi"; }
+
+	void move(int** map, int maxx, int maxy) {
+		int dir = rand() % 2;
+		int jump = rand() % 2 + 1;
+		if (nMove < 100) {
+			x += ((dir == 0) ? -jump : jump);
+			nMove++;
+		}
+		else if (nMove >= 100 && nMove < 200) {
+			y += ((dir == 0) ? -jump : jump);
+			nMove++;
+		}
+		else if (nMove >= 200 && nMove < 300) {
+			x += ((dir == 0) ? -jump : jump);
+			nMove++;
+		}
+		else if (nMove >= 300 && nMove < 400) {
+			y += ((dir == 0) ? -jump : jump);
+			nMove++;
+		}
+		else if (nMove >= 400 && nMove < 500) {
+			x += ((dir == 0) ? -jump : jump);
+			nMove++;
+		}
+		else x += ((dir == 0) ? -jump : jump);
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
+
+class Mymonster : public Smombi {
+public:
+	Mymonster(string n = "내몬스터", string i = "●", int x = 0, int y = 0)
+		: Smombi(n, i, x, y) {}
+	~Mymonster() { cout << " Mymonster"; }
+
+	void move(int** map, int maxx, int maxy) {
+		int dir = rand() % 8;
+		if (dir == 0) { x--; y--; }
+		else if (dir == 1) { x++; y++; }
+		else if (dir == 2) { x++; y--; }
+		else if (dir == 3) { x--; y++; }
+		else if (dir == 4) x--; else if (dir == 5) x++;
+		else if (dir == 6) y--; else y++;
+		clip(maxx, maxy);
+		eat(map);
+	}
+};
